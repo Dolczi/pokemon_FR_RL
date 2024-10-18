b7bd89c1cec624b37545b7f45799ecf669a59f87
Dolczi <jakub.dolecki2000@gmail.com>
2024-10-18 17:49:14 +0200

    Update env.py

--- env.py
+++ env.py
@@ -2,3 +2,13 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 from pyboy import PyBoy
+
+actions = ['','a', 'b', 'left', 'right', 'up', 'down', 'start', 'select']
+
+class PokemonRedEnv(gym.Env):
+
+    def __init__(self, pyboy, debug=False):
+        super().__init__()
+        self.pyboy = pyboy
+        self.debug = debug
+        
